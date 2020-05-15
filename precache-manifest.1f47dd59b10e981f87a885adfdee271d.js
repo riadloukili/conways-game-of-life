8ba46a008ba53b6de1572c8c7fd1d3e5ab453667
Riad Loukili <riadloukili@icloud.com>
2020-05-15 05:56:28 +0000

    Updates

---
+++ precache-manifest.1f47dd59b10e981f87a885adfdee271d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdb9d4b1e565c93480b16cf7324e4f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "359313d00d85aa751482",
+    "url": "/static/css/main.2b324f8f.chunk.css"
+  },
+  {
+    "revision": "4f5536c5ffc6d2792796",
+    "url": "/static/js/2.fc99887f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fc99887f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359313d00d85aa751482",
+    "url": "/static/js/main.3a9383a5.chunk.js"
+  },
+  {
+    "revision": "d1bac09240914957ce7f",
+    "url": "/static/js/runtime-main.b610cad6.js"
+  }
+]);
